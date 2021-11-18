f95311314e0245a95f3a2e180701a68b3b469425
AshishThakur10 <sendashishthakur@gmail.com>
2021-11-18 18:24:33 +0530

    update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-// const userRoute = require("./routes/user");
+const userRoute = require("./models/User");
 const authRoute = require("./routes/auth");
 const productRoute = require("./routes/product");
 const cartRoute = require("./routes/cart");
@@ -25,7 +25,7 @@ mongoose.connect('mongodb+srv://Ash1:Ash1@cluster0.pbef3.mongodb.net/myFirstData
 app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoute);
-// app.use("/api/users", userRoute);
+app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
 app.use("/api/carts", cartRoute);
 app.use("/api/orders", orderRoute);
