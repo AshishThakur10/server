780b9c84d268bef3bfd702153a0845cd68075774
Ashish Thakur <sendashishthakur@gmail.com>
2021-12-11 22:42:03 +0530

    updated

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const authRoute = require("./routes/auth");
 const productRoute = require("./routes/product");
 const cartRoute = require("./routes/cart");
 const orderRoute = require("./routes/order");
-const stripeRoute = require("./routes/stripe");
 const cors = require("cors");
 
 dotenv.config();
@@ -29,7 +28,11 @@ app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
 app.use("/api/carts", cartRoute);
 app.use("/api/orders", orderRoute);
-app.use("/api/checkout", stripeRoute);
+
+
+app.use("/", function(req,res){
+  res.send("welcome to the server")
+})
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Backend server is running!");
